ff73e30d11bd6adba5fe3e538a64c8d04360d2f1
RODSONSSJ <rodsongb@hotmail.com>
2026-01-02 10:53:09 -0300

    Implementação do Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,10 +28,15 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
-        System.out.println("=== TESTE 3: seller insert ===");
+        System.out.println("=== TESTE 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 40000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("=== TESTE 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!!");
 
     }
 }
