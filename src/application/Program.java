5845f7825027f2e4932f91cf3be5130adca34bc9
RODSONSSJ <rodsongb@hotmail.com>
2026-01-02 09:33:23 -0300

    findByID implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,12 +9,10 @@ import entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        Departament obj = new Departament("Books", 1);
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
-        System.out.println();
-        System.out.println(obj);
+
     }
 }
